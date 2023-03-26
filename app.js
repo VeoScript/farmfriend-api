ba971b2b64f79eccfd1125b3aa8c07f3026602b1
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-03-26 22:10:36 +0800

    feat: add models in prisma schema (#1)

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
+const cors = require('cors');
 const createError = require('http-errors');
 const morgan = require('morgan');
+require('express-async-errors');
 require('dotenv').config();
 
 const app = express();
+const server = require('http').createServer(app);
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}))
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
 app.get('/', async (req, res, next) => {
-  res.send({ message: 'Awesome it works 🐻' });
+  res.send({ message: 'Welcome to FarmFriend API' });
 });
 
 app.use('/api', require('./routes/api.route'));
@@ -26,5 +35,5 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => console.log(`🚀 Server is running in port ${PORT}`));

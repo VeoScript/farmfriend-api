e354d2c29c0924babb794d55dfd5b355d7462108
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-03-29 20:58:32 +0800

    feat: add infinite scroll pagination in programs index controller (#7)

--- controllers/programs.controller.js
+++ controllers/programs.controller.js
@@ -12,16 +12,30 @@ class ProgramsController {
       })
       return
     }
-    
+
+    const limit = 10
+    const cursor = req.query.cursor ?? ''
+    const cursorObj = cursor === '' ? undefined : { id: String(cursor) }
+
     try {
       const programs = await prisma.program.findMany({
         where: {
           title: {
             contains: req.query.search
           }
-        }
+        },
+        orderBy: {
+          created_at: 'desc'
+        },
+        take: limit,
+        cursor: cursorObj,
+        skip: cursor === '' ? 0 : 1
       })
-      res.status(200).json(programs)
+
+      res.status(200).json({
+        programs,
+        nextId:  programs.length === limit ? programs[limit - 1].id : undefined
+      })
     } catch (e) {
       next(createError(e.statusCode, e.message))
       process.exit(1)

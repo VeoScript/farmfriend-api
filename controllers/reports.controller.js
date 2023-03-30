058d6f8aa5fcf8b231da43e98daf7b7d551494e5
Gew <110228800+gewgew02@users.noreply.github.com>
2023-03-30 21:24:17 +0800

    feat: add create reports controller and api route
    Co-Authored-By: Jerome Villaruel <jeromevillaruel1998@gmail.com>

---
+++ controllers/reports.controller.js
@@ -0,0 +1,69 @@
+const createError = require("http-errors");
+require('express-async-errors');
+
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+class ReportsController {
+	static index = async (req, res, next) => {
+		// if (req.session.user === undefined) {
+    //   res.status(401).json({
+    //     message: 'Unauthorized!'
+    //   })
+    //   return
+    // }
+
+    const limit = 10
+    const cursor = req.query.cursor ?? ''
+    const cursorObj = cursor === '' ? undefined : { id: String(cursor) }
+
+    try {
+      const reports = await prisma.report.findMany({
+        where: {
+          description: {
+            contains: req.query.search
+          }
+        },
+        orderBy: {
+          created_at: 'desc'
+        },
+        take: limit,
+        cursor: cursorObj,
+        skip: cursor === '' ? 0 : 1
+      })
+
+      res.status(200).json({
+        reports,
+        nextId:  reports.length === limit ? reports[limit - 1].id : undefined
+      })
+    } catch (e) {
+      next(createError(e.statusCode, e.message))
+      process.exit(1)
+    }
+	};
+
+	static create = async (req, res, next) => {
+    if (req.session.user === undefined) {
+      res.status(401).json({
+        message: 'Unauthorized!'
+      })
+      return
+    }
+
+    try {
+      const createReport = await prisma.report.create({
+        data: {
+          type: req.body.type,
+					description: req.body.description,
+					user_id: req.body.user_id
+        }
+      })
+      res.status(200).json(createReport)
+    } catch (e) {
+      next(createError(e.statusCode, e.message))
+      process.exit(1)
+    }
+  };
+};
+
+module.exports = ReportsController;
